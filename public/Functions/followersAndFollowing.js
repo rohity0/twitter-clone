3a15ed9b14eb1c6d277f76da88beedc9a4e52670
rohit yadav <yadavry09@gmail.com>
2024-03-19 20:04:45 +0530

    follow-fix

--- public/Functions/followersAndFollowing.js
+++ public/Functions/followersAndFollowing.js
@@ -1,10 +1,8 @@
 $(document).ready(() => {
   if (selectedTab === "followers") {
-    console.log("efoeofjeofonononon");
-    loadFollowers();
+        loadFollowers();
   } else {
-    console.log("reac");
-    loadFollowing();
+        loadFollowing();
   }
 });
 
@@ -20,19 +18,42 @@ function loadFollowing() {
   });
 }
 
-function outputUser(data, container) {
+function outputUser(results, container) {
       container.html("");
-      data.forEach(result => {
-             
-      });
+      results.forEach((result) => {
+    let html = createUserHtml(result, true);
+    container.append(html);
+  });
+
+  if (results.length === 0) {
+    container.append("<span class='noResults'>No result found</span>");
+  }
 }
 
-
-function createUserHtml(userData, showFollowButton){
+function createUserHtml(userData, showFollowButton) {
+  let name = userData.firstName + " " + userData.lastName;
+  console.log(userLoggedIn);
+  let isFollowing =
+    userLoggedIn.following && userLoggedIn.following.includes(userData._id);
+  let text = isFollowing ? "Following" : "Follow";
+  let buttonClass = isFollowing ? "followButton following" : "followButton";
+  let followButton = "";
+  if (showFollowButton && userLoggedIn._id != userData._id) {
+    followButton = `<div>
+   <button class='${buttonClass}' data-user='${userData._id}'>${text}</button>
+ </div>`;
+  }
       return `<div class='user'>
                <div class ='userImageContainer'> 
-                 <img src='${userData.profilePic}' />
+                 <img src='${userData.profile}' />
+</div>
+               <div class ='userDetailsContainer'>
+                 <div class="header"> 
+                 <a href="/profile/${userData.userName}">${name}</a>
+                 <span class="userName">@${userData.userName}</span>
                </div>
                </div>
-      `
+      ${followButton}
+               </div>
+      `;
 }
